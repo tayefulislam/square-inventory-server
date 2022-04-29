708d662d37fbe0452135fd7eff4877f6a7209267
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-30 02:25:33 +0600

    initiazile the server side

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+const app = express()
+
+
+// middleware 
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+// apis
+
+app.get('/', (req, res) => {
+    res.send('server is runing')
+})
+
+app.listen(port,
+    console.log('server is runnig ar ', port)
+)
