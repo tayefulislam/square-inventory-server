ffb86464907acbd5aa3fc4869445b9e35e138a9f
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-30 04:51:49 +0600

    mongo server side added with problem

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
-const app = express()
+const app = express();
 
 
 // middleware  
@@ -9,6 +11,43 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fgmis.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+
+
+    try {
+
+        await client.connect()
+
+        const itemsCollection = client.db("squareInventory").collection("items");
+
+
+        app.get('/items', async (req, res) => {
+            const query = {}
+            const result = await itemsCollection.find(query);
+            res.send(result)
+        })
+
+
+    }
+    finally {
+
+    }
+
+}
+
+
+run().catch(console.dir)
 
 
 
