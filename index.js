4940e750dda62ef8762e787d2bca13635560a4c6
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-30 13:32:39 +0600

    get item by id funtion added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -26,6 +26,9 @@ async function run() {
 
 
 
+
+
+
         app.get('/items', async (req, res) => {
 
             const query = {};
@@ -37,6 +40,25 @@ async function run() {
             res.send(result);
 
         })
+
+
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id
+
+            const query = { _id: ObjectId(id) }
+
+            const result = await itemsCollections.findOne(query)
+
+            res.send(result)
+            console.log(id)
+
+
+        })
+
+
+
+
+
 
 
     }
